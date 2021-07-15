2eced348488eaf37fa8aa467349957faa94bf81d
AnnaLasmane <anna.gravere@gmail.com>
2021-07-15 18:35:04 +0300

    icon works

--- src/Components/Aboutus/Mission.js
+++ src/Components/Aboutus/Mission.js
@@ -1,8 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
 function Mission() {
     return (
         <div className="row mt-3">
             <div className="col">
                 <p> To be the best!</p>
+                <button>
+                <FontAwesomeIcon icon={faTrashAlt} /></button>
             </div>       
     
         </div>
