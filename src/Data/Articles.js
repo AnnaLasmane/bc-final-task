8eca19fc287f3eac2197e17e41c45bd8b4bd5c83
AnnaLasmane <anna.gravere@gmail.com>
2021-07-22 19:22:26 +0300

    text edited

--- src/Data/Articles.js
+++ src/Data/Articles.js
@@ -1,43 +1,43 @@
 const articles = [{
         title: 'Rocky',
         image: 'https://media.thetab.com/blogs.dir/20/files/2013/01/rockybalboa1.jpg',
-        text: 'Picking up immediately after the events in the first Rocky movie, Rocky II begins with the events of 14th round of the Bicentennial New Years Day fight from the first movie. The fight lasts the entire 15 rounds with each fighter suffering many devastating injuries. After the fight, Rocky calls out for Adrian, who runs down to the ring. As Creed is announced the winner by split decision, Adrian and Rocky embrace, and Rocky tells her that he loves her.After their championship bout, Rocky (Sylvester Stalone) and Apollo (Carl Weathers) are taken by ambulance to Pennsylvania Hospital in Center City (a WCAU-TV Channel 10 news truck follows behind the ambulance), where they meet once again in the hospital lobby with a mob of reporters. Apollo Creed, who retained the championship on points, is furious at Rocky, the first man to go the distance with him, and starts clamoring for another chance to knock Rocky out. Rocky is taken into surgery where the doctor will fix his broken nose. He tells Rocky he may need eye surgery to repair a detached retina.Rocky goes to Apollos room that night and asks Did you give me your best? to which Apollo answers "Yeah." Rocky declines talk of a rematch and tells Apollo that he is quitting boxing.Following his hospital stay, Rocky proposes to Adrian (Talie Shire) in front of the tiger paddock at the Philadelphia Zoo, and they later get married in a private church ceremony. Rocky begins to enjoy the profits from his fight with Apollo. He buys his first brand new car, a 1979 Pontiac Trans Am (even though its still supposedly 1976), goes on a shopping spree (purchasing a black leather jacket with a tiger on the back for himself.',
+        text: 'Picking up immediately after the events in the first Rocky movie, Rocky II begins with the events of 14th round of the Bicentennial New Years Day fight from the first movie. The fight lasts the entire 15 rounds with each fighter suffering many devastating injuries. After the fight, Rocky calls out for Adrian, who runs down to the ring. As Creed is announced the winner by split decision, Adrian and Rocky embrace, and Rocky tells her that he loves her.After their championship bout, Rocky (Sylvester Stalone) and Apollo (Carl Weathers) are taken by ambulance to Pennsylvania Hospital in Center City (a WCAU-TV Channel 10 news truck follows behind the ambulance), where they meet once again in the hospital lobby with a mob of reporters. Apollo Creed, who retained the championship on points, is furious at Rocky, the first man to go the distance with him, and starts clamoring for another chance to knock Rocky out. Rocky is taken into surgery where the doctor will fix his broken nose. He tells Rocky he may need eye surgery to repair a detached retina.',
         id: 'first-article',
         images: ['https://img.olhardigital.com.br/wp-content/uploads/2021/03/rocky-balboa-sylvester-stallone-e1588938141858.jpg',
-        'https://news24viral.com/wp-content/uploads/2021/05/1622063154_media.jpg', 
-        'https://www.gannett-cdn.com/-mm-/ea8a07dc617309ca168e259d006a72abae509118/c=0-35-650-402/local/-/media/2016/02/25/SiouxFalls/SiouxFalls/635919633708862216-rocky.jpg?auto=webp&format=pjpg&width=1200',
-        'https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/brrdzp5b5esporqx_1600253845.jpeg',
-        'https://imgresizer.eurosport.com/unsafe/1200x0/filters:format(jpeg):focal(1272x446:1274x444)/origin-imgresizer.eurosport.com/2018/08/10/2395438-49807530-2560-1440.jpg', 
-        'https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/mgm-AZ_02984M-Full-Image_GalleryBackground-en-US-1580966847283._SX1080_.jpg' ]},
-        
-    
-        
- 
+            'https://news24viral.com/wp-content/uploads/2021/05/1622063154_media.jpg',
+            'https://www.gannett-cdn.com/-mm-/ea8a07dc617309ca168e259d006a72abae509118/c=0-35-650-402/local/-/media/2016/02/25/SiouxFalls/SiouxFalls/635919633708862216-rocky.jpg?auto=webp&format=pjpg&width=1200',
+            'https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/brrdzp5b5esporqx_1600253845.jpeg',
+            'https://imgresizer.eurosport.com/unsafe/1200x0/filters:format(jpeg):focal(1272x446:1274x444)/origin-imgresizer.eurosport.com/2018/08/10/2395438-49807530-2560-1440.jpg',
+            'https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/mgm-AZ_02984M-Full-Image_GalleryBackground-en-US-1580966847283._SX1080_.jpg'
+        ]
+    },
     {
         title: 'Rambo',
         image: 'https://www.maxim.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTM1NjcxNjg2NjM1Nzc4MDE4/np010616_rambo_articlejpg.jpg',
         text: 'John Rambo (Sylvester Stallone) is a former member of an elite United States Army Special Forces unit and was awarded the Medal of Honor for his service in the Vietnam War in which he was also a prisoner of war (POW). The film begins 10 years after Rambo left the war, in the American Northwest in 1980. Rambo is searching for one of his friends from his unit, Delmar Berry. Rambo soon learns from Delmars mother that he has died a few months earlier from cancer due to Agent Orange exposure. Although not yet revealed to the audience, Rambo knows he is now the last surviving member of his unit. He leaves the picture of his unit with Berrys mother and walks away, obviously saddened.The scene fades to Rambo entering the small town of Hope, Washington State on foot, looking for a meal and possibly a hotel to spend the night. With his longish hair and military-style coat, he is quickly spotted by the towns overzealous and paranoid sheriff, Will Teasle (Brian Dennehy) who quickly drives Rambo out of town, noting his strong distaste for "drifters." A defiant Rambo heads back toward town immediately, to the dismay of Teasle who arrests him for vagrancy and for carrying a large combat knife as a concealed weapon.Rambo stands his ground against the officers at the station and is brutalized and harassed by Art Galt (Jack Starrett). ',
         id: 'second-article',
         images: ['https://i.pinimg.com/originals/64/e5/d6/64e5d6d01d55d1718a7e8cdc4c3bb311.jpg',
-         'https://i.pinimg.com/originals/ef/cb/6b/efcb6b29d768fb6199832d1d5c9bd805.jpg', 
-         'https://i.ytimg.com/an/-T39_xJo9iA/a90a1f89-6fe6-470c-aec6-2fa41c7fc1ab_mq.jpg?v=5ef6178d',
-         'https://sylvesterstallone.com/ia5/wp-content/uploads/2010/08/rambo4.jpg',
-         'https://upload.wikimedia.org/wikipedia/en/thumb/a/a3/Rambo3poster.jpg/220px-Rambo3poster.jpg', 
-         'https://m.media-amazon.com/images/M/MV5BZTQ4NjhkZWEtODY1MC00MzllLTkzNDYtZWVkMjNlNWRiOTdjXkEyXkFqcGdeQXVyMTAzMDM4MjM0._V1_.jpg' ]},
-       
-   
+            'https://i.pinimg.com/originals/ef/cb/6b/efcb6b29d768fb6199832d1d5c9bd805.jpg',
+            'https://i.ytimg.com/an/-T39_xJo9iA/a90a1f89-6fe6-470c-aec6-2fa41c7fc1ab_mq.jpg?v=5ef6178d',
+            'https://sylvesterstallone.com/ia5/wp-content/uploads/2010/08/rambo4.jpg',
+            'https://upload.wikimedia.org/wikipedia/en/thumb/a/a3/Rambo3poster.jpg/220px-Rambo3poster.jpg',
+            'https://m.media-amazon.com/images/M/MV5BZTQ4NjhkZWEtODY1MC00MzllLTkzNDYtZWVkMjNlNWRiOTdjXkEyXkFqcGdeQXVyMTAzMDM4MjM0._V1_.jpg'
+        ]
+    },
     {
         title: 'Assassins',
         image: 'http://basementrejects.com/wp-content/uploads/2015/07/assassins-movie-miguel-robert-cab-scene-taxi-sylvester-stallone-antonio-banderas-review.jpg',
         text: 'Robert Rath (Sylvester Stallone) is known as the number one paid assassin in the world. However, after retiring (killing) a "mark" (target) who used to be an assassin himself, hes more and more heavily considering retirement for himself as he is tired of his job and its lifestyle. He also feels quite lonely since he lost his best friend and mentor Nicolai Tashlinkov, who was murdered 15 years ago.',
         id: 'third-article',
         images: ['http://theactionelite.com/wp-content/uploads/2018/01/assassins-movie-miguel-robert-ending-stand-off-antonio-banderas-sylvester-stallone-review.jpg',
-         'http://theactionelite.com/wp-content/uploads/2021/07/a5163022bcd647ec3409858aaca4d06eee9d046a.jpg', 
-         'https://3.bp.blogspot.com/-jZAjVTyxDDA/WQft08rowuI/AAAAAAAARqU/nHF0rch0FV0zSfIHNOcrg5Tpx36hAAthACLcB/w1200-h630-p-k-no-nu/Assassins%2B1.png',
-         'https://cdn10.bigcommerce.com/s-x8dfmo/products/613/images/23672/Sylvester-Stallone-%2526-Antonio-Banderas-in-Assassins-Premium-Photograph-and-Poster-1000641__65259.1432416112.1280.1280.jpg?c=2',
-         'https://i.pinimg.com/736x/71/32/20/713220614413bc5c0734e8650724ff35.jpg',
-         'https://images.static-bluray.com/reviews/4693_1.jpg'] },
- 
+            'http://theactionelite.com/wp-content/uploads/2021/07/a5163022bcd647ec3409858aaca4d06eee9d046a.jpg',
+            'https://3.bp.blogspot.com/-jZAjVTyxDDA/WQft08rowuI/AAAAAAAARqU/nHF0rch0FV0zSfIHNOcrg5Tpx36hAAthACLcB/w1200-h630-p-k-no-nu/Assassins%2B1.png',
+            'https://cdn10.bigcommerce.com/s-x8dfmo/products/613/images/23672/Sylvester-Stallone-%2526-Antonio-Banderas-in-Assassins-Premium-Photograph-and-Poster-1000641__65259.1432416112.1280.1280.jpg?c=2',
+            'https://i.pinimg.com/736x/71/32/20/713220614413bc5c0734e8650724ff35.jpg',
+            'https://images.static-bluray.com/reviews/4693_1.jpg'
+        ]
+    },
+
 ];
 
 export default articles;
